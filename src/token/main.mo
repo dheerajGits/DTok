f88446a48fde7808e6c85149533b7657393d3d41
Dheeraj Sharma <dheerajsadan2002@gmail.com>
2024-08-27 16:56:32 +0530

    add faucet functionality and implement functionalities in frontend

--- src/token/main.mo
+++ src/token/main.mo
@@ -1,5 +1,6 @@
 import Principal "mo:base/Principal";
 import HashMap "mo:base/HashMap";
+import Debug "mo:base/Debug";
 actor Token {
     var owner : Principal = Principal.fromText("i3sui-4jlhl-x5foh-vozkm-nq4uw-yqh4y-esg6g-wpylg-kf7a5-r2t3r-uae");
     var totalSupply : Nat = 1000000000; // total token
@@ -15,5 +16,19 @@ actor Token {
 
         return balance;
     };
-
+    public query func getCurrencySymbol() : async Text {
+        return symbol;
+    };
+    public shared (msg) func payOut() : async Text {
+        let caller = msg.caller;
+        Debug.print(debug_show (msg.caller));
+        let amount = 10000;
+        let balance = await balanceOf(caller);
+        if (balance != 0) {
+            balances.put(msg.caller, amount +balance);
+        } else {
+            balances.put(msg.caller, amount);
+        };
+        return "Sucess";
+    };
 };
--- src/token_assets/src/components/Faucet.jsx
+++ src/token_assets/src/components/Faucet.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { token } from "../../../declarations/token";
+import { Iso } from "@material-ui/icons";
 
 function Faucet() {
 
+  const [isLoading, setIsLoading] = useState(false);
   async function handleClick(event) {
-
+    setIsLoading(true);
+    const message = await token.payOut();
+    setIsLoading(false);
   }
 
   return (
@@ -14,9 +19,9 @@ function Faucet() {
         </span>
         Faucet
       </h2>
-      <label>Get your free DAngela tokens here! Claim 10,000 DANG coins to your account.</label>
+      <label>Get your free DTOK tokens here! Claim 10,000 DTOK coins to your account.</label>
       <p className="trade-buttons">
-        <button id="btn-payout" onClick={handleClick}>
+        <button id="btn-payout" onClick={handleClick} disabled={isLoading}>
           Gimme gimme
         </button>
       </p>
